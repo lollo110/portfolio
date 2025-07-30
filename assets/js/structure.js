a3e93df46873a09f5f81ec387ec3627d0cb10dc0
lollo110 <lorenzopaolocilurzo@gmail.com>
2025-07-30 16:53:45 +0200

    portfolio

---
+++ assets/js/structure.js
@@ -0,0 +1,65 @@
+// Cursor qui suive avec lumiere
+let spotlight = document.querySelector('.spotlight');
+
+document.addEventListener('mousemove', (e) => {
+    spotlight.style.setProperty('--x', `${e.clientX}px`);
+    spotlight.style.setProperty('--y', `${e.clientY}px`);
+})
+
+// Ingrandir les image
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Cambiato il selettore per puntare solo alle immagini con classe 'bigger'
+    let clickableThumbnails = document.querySelectorAll('.bigger'); 
+    
+    let modal = document.getElementById('imageModal');
+    let modalImage = document.getElementById('modalImage');
+    let closeButton = document.querySelector('.close-button');
+    let body = document.body;
+
+    // Funzione per calcolare la larghezza della scrollbar (per evitare lo sbalzo della pagina)
+    function getScrollbarWidth() {
+        let outer = document.createElement('div');
+        outer.style.visibility = 'hidden';
+        outer.style.overflow = 'scroll';
+        outer.style.msOverflowStyle = 'scrollbar';
+        document.body.appendChild(outer);
+        let inner = document.createElement('div');
+        outer.appendChild(inner);
+        let scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+        outer.parentNode.removeChild(outer);
+        return scrollbarWidth;
+    }
+
+    let scrollbarWidth = getScrollbarWidth();
+    document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
+
+
+    clickableThumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            modal.classList.add('active');
+            modalImage.src = thumbnail.dataset.fullSrc;
+            body.classList.add('no-scroll');
+        });
+    });
+
+    let closeModal = () => {
+        modal.classList.remove('active');
+        body.classList.remove('no-scroll');
+    };
+
+    closeButton.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape' && modal.classList.contains('active')) {
+            closeModal();
+        }
+    });
+});
