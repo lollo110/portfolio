b87a740670ac7e4e2fcbaac95f73a40717ac9e09
lollo110 <lorenzopaolocilurzo@gmail.com>
2025-07-31 14:38:07 +0200

    portfolio aggiornato

--- assets/js/structure.js
+++ assets/js/structure.js
@@ -1,63 +1,64 @@
 // Cursor qui suive avec lumiere
-let spotlight = document.querySelector('.spotlight');
+let spotlight = document.querySelector(".spotlight");
 
-document.addEventListener('mousemove', (e) => {
-    spotlight.style.setProperty('--x', `${e.clientX}px`);
-    spotlight.style.setProperty('--y', `${e.clientY}px`);
-})
+document.addEventListener("mousemove", (e) => {
+  spotlight.style.setProperty("--x", `${e.clientX}px`);
+  spotlight.style.setProperty("--y", `${e.clientY}px`);
+});
 
 // Ingrandir les image
 
+document.addEventListener("DOMContentLoaded", () => {
+  let clickableThumbnails = document.querySelectorAll(".bigger");
 
-document.addEventListener('DOMContentLoaded', () => {
-    let clickableThumbnails = document.querySelectorAll('.bigger'); 
-    
-    let modal = document.getElementById('imageModal');
-    let modalImage = document.getElementById('modalImage');
-    let closeButton = document.querySelector('.close-button');
-    let body = document.body;
+  let modal = document.getElementById("imageModal");
+  let modalImage = document.getElementById("modalImage");
+  let closeButton = document.querySelector(".close-button");
+  let body = document.body;
 
-    function getScrollbarWidth() {
-        let outer = document.createElement('div');
-        outer.style.visibility = 'hidden';
-        outer.style.overflow = 'scroll';
-        outer.style.msOverflowStyle = 'scrollbar';
-        document.body.appendChild(outer);
-        let inner = document.createElement('div');
-        outer.appendChild(inner);
-        let scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
-        outer.parentNode.removeChild(outer);
-        return scrollbarWidth;
+  function getScrollbarWidth() {
+    let outer = document.createElement("div");
+    outer.style.visibility = "hidden";
+    outer.style.overflow = "scroll";
+    outer.style.msOverflowStyle = "scrollbar";
+    document.body.appendChild(outer);
+    let inner = document.createElement("div");
+    outer.appendChild(inner);
+    let scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+    outer.parentNode.removeChild(outer);
+    return scrollbarWidth;
+  }
+
+  let scrollbarWidth = getScrollbarWidth();
+  document.documentElement.style.setProperty(
+    "--scrollbar-width",
+    `${scrollbarWidth}px`
+  );
+
+  clickableThumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", () => {
+      modal.classList.add("active");
+      modalImage.src = thumbnail.dataset.fullSrc;
+      body.classList.add("no-scroll");
+    });
+  });
+
+  let closeModal = () => {
+    modal.classList.remove("active");
+    body.classList.remove("no-scroll");
+  };
+
+  closeButton.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
     }
+  });
 
-    let scrollbarWidth = getScrollbarWidth();
-    document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
-
-
-    clickableThumbnails.forEach(thumbnail => {
-        thumbnail.addEventListener('click', () => {
-            modal.classList.add('active');
-            modalImage.src = thumbnail.dataset.fullSrc;
-            body.classList.add('no-scroll');
-        });
-    });
-
-    let closeModal = () => {
-        modal.classList.remove('active');
-        body.classList.remove('no-scroll');
-    };
-
-    closeButton.addEventListener('click', closeModal);
-
-    modal.addEventListener('click', (event) => {
-        if (event.target === modal) {
-            closeModal();
-        }
-    });
-
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape' && modal.classList.contains('active')) {
-            closeModal();
-        }
-    });
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && modal.classList.contains("active")) {
+      closeModal();
+    }
+  });
 });
